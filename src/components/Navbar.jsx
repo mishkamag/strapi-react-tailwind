9333942b77e6ca12d2657bad9f36d835fbc4db76
mishkamag <m93.maglaperidze@gmail.com>
2023-04-14 00:56:37 +0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-screen h-[80px] z-10 fixed bg-white shadow-lg ">
+      <div className="flex justify-between items-center h-full max-w-[1240px] m-auto  md:bg-green-500 ">
+        <div className="flex items-center">hi</div>
+        <div>hi</div>
+        <div>hi</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
