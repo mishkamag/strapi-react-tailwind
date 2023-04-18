d82d3add8a1aa0424f8b44375e171e1743f225d0
mishkamag <m93.maglaperidze@gmail.com>
2023-04-18 14:01:17 +0400

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { AiFillCloseSquare } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
   return (
     <div className="w-screen h-[80px] z-10 fixed bg-white shadow-lg ">
       <div className="flex justify-between items-center w-full h-full md:max-w-[1240px] m-auto  ">
-        <div className="sm:ml-10 ss:ml-10 opacity-[50px]">LOGO</div>
+        <div className="sm:ml-10 ss:ml-10 opacity-[50px]">LOGOOO</div>
 
         <div className="flex items-center">
           <ul className="hidden md:flex">
@@ -25,22 +27,37 @@ const Navbar = () => {
           <button className="px-10 py-3">Sing Up</button>
         </div>
 
-        <div className="md:hidden ">
-          <GiHamburgerMenu className="text-2xl " />
+        <div
+          className="md:hidden "
+          onClick={() => {
+            setMenu(!menu);
+          }}
+        >
+          {!menu ? (
+            <GiHamburgerMenu className="text-2xl " />
+          ) : (
+            <AiOutlineClose className="text-2xl " />
+          )}
         </div>
-
-        <ul className="absolute bg-white w-full px-8 shadow-lg md:hidden ">
-          <li>Home</li>
-          <li>About</li>
-          <li>Support</li>
-          <li>Platform</li>
-          <li>Pricing</li>
-          <div className="flex flex-col my-4 gap-2">
-            <button className=" bg-transparent text-black py-3 ">Login</button>
-            <button className="px-10 py-3">Sing Up</button>
-          </div>
-        </ul>
       </div>
+
+      <ul
+        className={
+          menu
+            ? "absolute bg-white w-full px-8 shadow-lg md:hidden flex flex-col justify-center items-center"
+            : "hidden"
+        }
+      >
+        <li>Home</li>
+        <li>About</li>
+        <li>Support</li>
+        <li>Platform</li>
+        <li>Pricing</li>
+        <div className="flex flex-col my-4 gap-2 w-full">
+          <button className=" bg-transparent text-black py-3 ">Login</button>
+          <button className="px-10 py-3">Sing Up</button>
+        </div>
+      </ul>
     </div>
   );
 };
