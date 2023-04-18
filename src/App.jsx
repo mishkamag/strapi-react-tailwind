586b51cc1187b26c0f0c2a6513ce6674b7945621
mishkamag <m93.maglaperidze@gmail.com>
2023-04-18 19:33:58 +0400

    finished footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Blogs from "./components/Blogs";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div>
       <Navbar />
       <Blogs />
+      <Footer />
     </div>
   );
 }
