f6ede6d99dcb1932f40f541321dd85473439c1a0
mishkamag <m93.maglaperidze@gmail.com>
2023-04-19 20:29:46 +0400

    working routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import BlogDetails from "./pages/BlogDetails";
 import Homepage from "./pages/Homepage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      {/* <Homepage /> */}
-      <BlogDetails />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/blog/:id" element={<BlogDetails />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Blogs = () => {
   const blogs = [
@@ -30,20 +31,22 @@ const Blogs = () => {
       <div className="max-w-[1240px] mx-auto">
         <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 gap-8 px-4 text-black">
           {blogs.map((blog) => (
-            <div
-              key={blog.id}
-              className="bg-white rounded-xl overflow-hidden drop-shadow-md"
-            >
-              <img
-                src={blog.coverImg}
-                alt=""
-                className="h-56 w-full object-cover"
-              />
-              <div className="p-8">
-                <h3 className="font-bold text-2xl my-1">{blog.title}</h3>
-                <p className="text-gray-600 text-xl ">{blog.desc}</p>
+            <Link to={`/blog/${blog.id}`}>
+              <div
+                key={blog.id}
+                className="bg-white rounded-xl overflow-hidden drop-shadow-md"
+              >
+                <img
+                  src={blog.coverImg}
+                  alt=""
+                  className="h-56 w-full object-cover"
+                />
+                <div className="p-8">
+                  <h3 className="font-bold text-2xl my-1">{blog.title}</h3>
+                  <p className="text-gray-600 text-xl ">{blog.desc}</p>
+                </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
